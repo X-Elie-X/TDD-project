57f511ea7dd4113cb7087a487b5fd472f798714d
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-11-25 22:35:26 +0200

    add Solver

--- solver.rb
+++ solver.rb
@@ -1,1 +1,11 @@
-
+class Solver
+    def factorial(num)
+        if num.zero?
+        1
+        elsif num.negative?
+        raise 'Exception Created: Factorial ERROR Number is Negative'
+        else
+        num * factorial(num - 1)
+        end
+    end
+end
